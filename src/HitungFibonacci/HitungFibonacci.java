ff35eb93e94177597c03036b5468be634d2c21ad
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-09-09 17:16:00 +0700

    Tambah Input

--- src/HitungFibonacci/HitungFibonacci.java
+++ src/HitungFibonacci/HitungFibonacci.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package HitungFibonacci;
-
+import java.util.Scanner;
 /**
  *
  * @author WINDOWS 10
@@ -13,6 +13,8 @@ public class HitungFibonacci {
     public static void main(String[] args) {
         String identitas = "Nadila Amalia Pribadi / XRPL3 / 29";
         tampilJudul(identitas);
+        
+        int n = tampilInput();
     }
     
     private static void tampilJudul(String identitas) {
@@ -20,5 +22,14 @@ public class HitungFibonacci {
         
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ...dst.\n");
+    }
+    
+    private static int tampilInput() {
+        Scanner baca = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke- ");
+        int n = baca.nextInt();
+        
+        return n;
     }
 }
