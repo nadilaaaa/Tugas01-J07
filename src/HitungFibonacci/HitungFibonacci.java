6d2431ca2cdd6fea7a89d9f44798fc319c6218ea
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-09-09 17:14:42 +0700

    Initial Commit

---
+++ src/HitungFibonacci/HitungFibonacci.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HitungFibonacci;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class HitungFibonacci {
+    public static void main(String[] args) {
+        String identitas = "Nadila Amalia Pribadi / XRPL3 / 29";
+        tampilJudul(identitas);
+    }
+    
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas: " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ...dst.\n");
+    }
+}
