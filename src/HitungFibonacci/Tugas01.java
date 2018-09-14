2988ea5d0c8406ded26a508fc2a8610283652613
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-09-14 21:15:21 +0700

    Proses dan Output

--- src/HitungFibonacci/Tugas01.java
+++ src/HitungFibonacci/Tugas01.java
@@ -21,5 +21,40 @@ public class Tugas01 {
         int a = 0, b = 0, c = 0, d = 0, e = 0;
         int[] nominal = new int[] {100, 500, 1000, 2000, 5000};
         
+        
+        while(uang >= 5000)
+        {
+            uang = uang-5000;
+            a++;
+        }
+        System.out.println("Banyak uang 5000 sebanyak: " + a);
+        
+        while(uang >= 2000)
+        {
+            uang = uang - 2000;
+            b++;
+        }
+        System.out.println("Banyak uang 2000 sebanyak: " + b);
+        
+        while(uang >= 1000)
+        {
+            uang = uang - 1000;
+            c++;
+        }
+        System.out.println("Banyak uang 1000 sebanyak: " + c);
+        
+        while(uang >= 500)
+        {
+            uang = uang - 500;
+            d++;
+        }
+        System.out.println("Banyak uang 500 sebanyak: " + d);
+        
+        while(uang >= 100)
+        {
+            uang = uang - 100;
+            e++;
+        }
+        System.out.println("Banyak uang 100 sebanyak: " + e);
     }
 }
