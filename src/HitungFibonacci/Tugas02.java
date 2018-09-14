4531d9802770125ad099d85cf3dc16667025ab3d
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-09-14 21:26:42 +0700

    Input

--- src/HitungFibonacci/Tugas02.java
+++ src/HitungFibonacci/Tugas02.java
@@ -14,6 +14,23 @@ public class Tugas02 {
         Scanner input = new Scanner(System.in);
         
         String identitas = "Nadila Amalia Pribadi/XRPL3/29";
+        
+        double total;
+        
+        System.out.println("1. A");
+        System.out.println("2. B");
+        System.out.println("3. C");
+        System.out.println("4. D");
+        System.out.println("5. E");
+        System.out.println("Jenis Barang: (1/2/3/4/5)");
+        double jenis = input.nextDouble();
+        
+        System.out.println("");
+        
+        System.out.print("Masukkan berat barang: ");
+        double berat = input.nextDouble();
+        
+        System.out.println("");
     }
 }
 
