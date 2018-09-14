562e9ee271729ab7aef1fd18bb5e9f8925cef952
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-09-14 21:27:44 +0700

    Proses dan output

--- src/HitungFibonacci/Tugas02.java
+++ src/HitungFibonacci/Tugas02.java
@@ -31,6 +31,72 @@ public class Tugas02 {
         double berat = input.nextDouble();
         
         System.out.println("");
+        
+        if(jenis == (1))
+        {
+            if(berat % 4 == 0)
+            {
+                total = berat/4*5000;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+            else
+            {
+                total = (5000/4)*berat;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+        }
+        else if(jenis == 2)
+        {
+            if(berat % 5 == 0)
+            {
+                total = berat/5*7000;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+            else
+            {
+                total = (7000/5)*berat;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+        }
+        else if(jenis == 3)
+        {
+            if(berat % 2 == 0)
+            {
+                total = berat/2*2000;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+            else
+            {
+                total = (2000/2)*berat;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+        }
+        else if(jenis == 4)
+        {
+            if(berat % 3 == 0)
+            {
+                total = berat/3*3000;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+            else
+            {
+                total = (3000/3)*berat;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+        }
+        else if(jenis == 5)
+        {
+            if(berat % 7 == 0)
+            {
+                total = berat/7*10000;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+            else
+            {
+                total = (10000/7)*berat;
+                System.out.println("Total yang harus dibayar: Rp." + total);
+            }
+        }
     }
 }
 
